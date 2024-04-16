1ed614d3a9f89bbe113dad5c41f895abcdcf3929
Iuri Ribeiro <iuri-mcd@hotmail.com>
2024-04-16 20:04:05 -0300

    gitgnore

--- MUNDO 2/repetições em py.py
+++
@@ -1,4 +0,0 @@
-'''Nessa aula, vamos começar nossos estudos com os laços e vamos fazer primeiro o “for”, que é uma estrutura versátil e simples de entender. Por exemplo:
-for c in range(0, 4): ATENÇÃO PARA OS DOIS PONTOS NO FINAL DO LAÇO
-print(c)
-print('Acabou')'''
